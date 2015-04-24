e59313eb379169ca8847762372aeca62f3c9fa5f
Shino1024 <justincompspec@gmail.com>
2015-04-24 23:03:45 +0200

    Resigned from automatic incrementation and postcrementation of digits.

--- include/SuperInteger.h
+++ include/SuperInteger.h
@@ -23,22 +23,10 @@ class SuperInteger {
 		operator unsigned long();
 		int digc() {return digits;}
 		std::deque<Digit> returnDeque() const {return integers;}
-		void ins(Digit a) {
-			integers.push_front(a);
-			++digits;
-		}
-		void push(Digit a) {
-			integers.push_back(a);
-			++digits;
-		}
-		void pop() {
-			integers.pop_front();
-			--digits;
-		}
-		void drop() {
-			integers.pop_back();
-			--digits;
-		}
+		void ins(Digit a) {integers.push_front(a);}
+		void push(Digit a) {integers.push_back(a);}
+		void pop() {integers.pop_front();}
+		void drop() {integers.pop_back();}
 		int count() const {return c;}
 		int& operator[](int a) {return integers.at(a - 1).x;}
 		SuperInteger(char[]);
